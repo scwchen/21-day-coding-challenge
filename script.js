6d0365b2410402c66beaf0aeb9ce1929820c20e9
Steven Chen <steven.chen.121@gmail.com>
2021-11-25 09:36:20 -0500

    Challenge #4 - Complete

--- script.js
+++ script.js
@@ -64,3 +64,28 @@ const checkGaugeStatus = (gauge) => {
         return false;
     }
 }
+
+// ===========================================
+// Challenge #4 - Hope you’re not afraid of the dark...
+// ===========================================
+
+// Create a function that takes in a toggle object and will change the value of the property isOn between true and false and return the updated object. Using the function twice should revert the toggle back to its original value.
+
+// Input:
+
+// const someToggle = {
+//     name: "toggleA",
+//     isOn: false
+// }
+
+// Output:
+
+// const someToggle = {
+//     name: "toggleA",
+//     isOn: true
+// }
+
+const switchToggle = (toggle) => {
+    toggle.isOn = !toggle.isOn;
+    return toggle;
+}
