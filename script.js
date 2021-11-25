1a1d784c4336614b9c5f1ea07261c2323bda2a64
Steven Chen <steven.chen.121@gmail.com>
2021-11-24 20:25:49 -0500

    First three days of the challenge.

---
+++ script.js
@@ -0,0 +1,66 @@
+// ===========================================
+// Challenge #1 - To compilation and beyond!
+// ===========================================
+
+
+// Your first task is to write a function that will take two string parameters and return a string containing the sender name and the message in the form specified in the instructions.
+
+// Input:
+// parseMessage("Mission Control", "Hello there!")
+
+// Output:
+// Mission Control: Hello there!
+
+
+const parseMessage = (origin, message) => {
+    return origin + ': ' + message;
+}
+// ===========================================
+// Challenge #2 - One small step at a time
+// ===========================================
+
+// Your task is to automate that process by creating a function that will take in an astronaut object as a parameter and return a string using the properties of the astronaut.
+
+// Input:
+// const exampleAstronaut = {
+//     firstName: "Yuri",
+//     lastName: "Gagarin",
+//     nickname: "First!",
+//     prefix: "Cosmonaut"
+// }
+
+// Output:
+// Cosmonaut: Yuri "First!" Gagarin
+
+const generateAstronautTag = (astronaut) => {
+    const { firstName, lastName, nickname, prefix } = astronaut;
+    const greeting = `${prefix}: ${firstName} "${nickname}" ${lastName}`;
+
+    return greeting;
+}
+
+// ===========================================
+// Challenge #3 - What’s in the box?
+// ===========================================
+
+// Your task is to create a function that will take the gauge object as a parameter, and will return true if the current value is between the minimum and maximum, and return false if the value is outside those.
+
+// Input:
+// const exampleGauge = {
+//     current: 0.4,
+//     min: 0.1,
+//     max: 0.9
+// }
+
+// Output:
+// true
+
+const checkGaugeStatus = (gauge) => {
+    const { current, min, max } = gauge;
+
+    if (current > min && current < max) {
+        return true;
+    } else {
+        return false;
+    }
+}
