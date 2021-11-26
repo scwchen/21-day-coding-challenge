484c8721a178012c3295c71e3e56449b82b91eb1
Steven Chen <steven.chen.121@gmail.com>
2021-11-26 02:28:38 -0500

    Challenge #5 Complete

--- script.js
+++ script.js
@@ -89,3 +89,31 @@ const switchToggle = (toggle) => {
     toggle.isOn = !toggle.isOn;
     return toggle;
 }
+
+// ===========================================
+// Challenge #5 - You get a job, she gets a job, he gets a job
+// ===========================================
+
+// Now that a crew has been selected, you need to write a new function that will assign a job to each astronaut. Since you’ve had some experience with updating object properties in some of the previous challenges, this should be fairly straightforward. Just keep in mind that you need to return the astronaut after updating the job property.
+
+// Input:
+// const exampleAstronaut = {
+//     firstName: "Chris",
+//     lastName: "Hadfield",
+//     nickname: "Space Oddity",
+//     prefix: "Astronaut"
+// }
+
+// Output:
+// const exampleAstronaut = {
+//     firstName: "Chris",
+//     lastName: "Hadfield",
+//     nickname: "Space Oddity",
+//     prefix: "Astronaut",
+//     job: "Shuttle DJ"
+// }
+
+const addJobToAstronaut = (astronaut, job) => {
+    astronaut.job = job;
+    return astronaut;
+}
