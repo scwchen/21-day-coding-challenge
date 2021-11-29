4fa0edfab54c36a20465d641b6cc099548e89c29
Steven Chen <steven.chen.121@gmail.com>
2021-11-29 10:31:46 -0500

    Challenge # 8

--- script.js
+++ script.js
@@ -177,3 +177,31 @@ const storeWeatherConditions = (temperature, condition, windSpeed, windDirection
         condition: condition,
     }
 }
+
+// ===========================================
+// Challenge #8 - It’s all part of the job
+// ===========================================
+
+// Your task is to create a function that takes in a roster array, and will calculate the amount of astronauts in the given roster list and return said amount.
+
+// Sometimes things are just simple, code doesn't need to be complicated!
+
+// Input:
+
+// const exampleRoster = [
+//     {
+//         firstName: "Chris",
+//         lastName: "Hadfield",
+//         nickname: "Space Oddity",
+//         prefix: "Astronaut",
+//         job: "Shuttle DJ"
+//     }
+// ]
+
+// Output:
+
+// 1
+
+const countActiveAstronauts = (roster) => {
+    return roster.length;
+}
