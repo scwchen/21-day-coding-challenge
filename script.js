75ede6d2e8d7f3313fa8721c6f656a93de70c1b4
Steven Chen <steven.chen.121@gmail.com>
2021-11-30 02:13:24 -0500

    Challenge #9

--- script.js
+++ script.js
@@ -205,3 +205,32 @@ const storeWeatherConditions = (temperature, condition, windSpeed, windDirection
 const countActiveAstronauts = (roster) => {
     return roster.length;
 }
+
+// ===========================================
+// Challenge #9 - Media mania!
+// ===========================================
+
+// Your task is to create a function that will take in an array representing the roster of astronauts, and return an array containing the jobs of each astronaut as a string.
+
+// In other terms, for each astronaut of the roster, you want to take the job and add it to a list, then return said list.
+
+// Input:
+
+// const exampleRoster = [
+//     {
+//         ...
+//         job: "Shuttle DJ"
+//       },
+//     {
+//         ...
+//         job: "Space Cook"
+//       }
+// ]
+
+// Output:
+
+// ["Shuttle DJ", "Space Cook"]
+
+const listAstronautJobs = (roster) => {
+    return roster.map(astronaut => astronaut.job);
+}
