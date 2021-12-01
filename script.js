29645efae94c7ab4f14eb6eb1a3c1554d4edce5a
Steven Chen <steven.chen.121@gmail.com>
2021-12-01 09:21:17 -0500

    Challenge #10

--- script.js
+++ script.js
@@ -234,3 +234,35 @@ const countActiveAstronauts = (roster) => {
 const listAstronautJobs = (roster) => {
     return roster.map(astronaut => astronaut.job);
 }
+
+// ===========================================
+// Challenge #10 - Flying solo
+// ===========================================
+
+// Your task is to create a function that will take in an array of weather objects and will return a rounded average of the wind speed.
+// Since we work with an unknown amount of entries, you will need to calculate the average wind speed using loops.
+
+//     Input:
+
+// const exampleEntries = [
+//     {
+//         temperature: 0,
+//         weather: "sunny",
+//         windDirection: "NNE",
+//         windSpeed: 24
+//     },
+//     {
+//         temperature: 10,
+//         weather: "cloudy",
+//         windDirection: "NNE",
+//         windSpeed: 9
+//     }
+// ]
+
+// Output:
+
+// 17
+
+const averageWindSpeed = (weatherEntries) => {
+    return Math.round(weatherEntries.reduce((total, entry) => total + entry.windSpeed, 0) / weatherEntries.length);
+}
