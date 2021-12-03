bd0815e1c580d0421e653efe94fffcf38f11e50f
Steven Chen <steven.chen.121@gmail.com>
2021-12-03 12:38:55 -0500

    Challenge #12

--- script.js
+++ script.js
@@ -321,3 +321,32 @@ const bookFreePlatform = (platformList, missionDate) => {
     } catch { }
     return platformList;
 }
+
+// ===========================================
+// Challenge #12 - From novice to master
+// ===========================================
+
+// This challenge is worth 20 points (who knows why)
+
+// Your task is to write a function that will take in an array of objects containing a sender and a message as a parameter.The function will then parse a message from each object, add it to an array then return the built array.
+
+// Each message is built exactly like the first challenge, so you can either use the function you already made, or rebuild it from scratch.
+
+//     Examples
+// Input:
+
+// const messages = [
+//     { origin: "MC", message: "Hello!" },
+//     { origin: "Shuttle", message: "Hey!" },
+// ]
+
+// Output:
+
+// [
+//     "MC: Hello!",
+//     "Shuttle: Hey!"
+// ]
+
+const parseTranscripts = (messages) => {
+    return messages.map(message => `${message.origin}: ${message.message}`);
+}
