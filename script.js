4562c5875740987688bf511988d9ce2f48082b5e
Steven Chen <steven.chen.121@gmail.com>
2021-12-04 11:28:16 -0500

    Challenge #13

--- script.js
+++ script.js
@@ -350,3 +350,37 @@ const bookFreePlatform = (platformList, missionDate) => {
 const parseTranscripts = (messages) => {
     return messages.map(message => `${message.origin}: ${message.message}`);
 }
+
+// ===========================================
+// Challenge #13 - One gauge at a time
+// ===========================================
+
+// Instructions
+
+// Your task is to write a function that will take in an array of objects containing a gauge reading(min, max, current).The function will then check if the gauge current value is within spec(between min and max) and check the next gauge.If one of the values is outside the spec, return false, if they are all ok, return true.
+
+// The method the evaluate the gauge is the same as the third challenge.You may take your function, get inspired by it, or start from scratch in this challenge.
+
+// Examples
+// Input:
+
+// const gaugeList = [
+//     {
+//         current: 0.4,
+//         min: 0.1,
+//         max: 0.9
+//     },
+//     {
+//         current: 1.2,
+//         min: 0.1,
+//         max: 0.6
+//     }
+// ]
+
+// Output:
+
+// false
+
+const checkAllGauges = (gaugeList) => {
+    return gaugeList.every(gauge => (gauge.current > gauge.min && gauge.current < gauge.max));
+}
