da3f15828698f118fb901de21ed09d686c375ebf
Steven Chen <steven.chen.121@gmail.com>
2021-12-05 10:22:29 -0500

    Challenge #14

--- script.js
+++ script.js
@@ -384,3 +384,46 @@ const parseTranscripts = (messages) => {
 const checkAllGauges = (gaugeList) => {
     return gaugeList.every(gauge => (gauge.current > gauge.min && gauge.current < gauge.max));
 }
+
+// ===========================================
+// Challenge #14 - List-o-mania
+// ===========================================
+
+// Instructions
+
+
+// Your task is to write a function that will take in an array of objects containing switches.The function will change the value of the isOn property to true for every switch in the list, and then return the updated array.
+
+// You can use the previous switchToggle function from the fourth challenge, however be careful, since we want all of them on, not toggled!
+
+// Examples
+// Input:
+
+// const toggleList = [
+//     {
+//         name: "Air",
+//         isOn: true
+//     },
+//     {
+//         name: "Radio",
+//         isOn: false
+//     },
+// ]
+
+// Output:
+
+// [
+//     {
+//         name: "Air",
+//         isOn: true
+//     },
+//     {
+//         name: "Radio",
+//         isOn: true
+//     },
+// ]
+
+const switchAllTogglesOn = (toggleList) => {
+    toggleList.forEach(toggle => toggle.isOn = true);
+    return toggleList;
+}
