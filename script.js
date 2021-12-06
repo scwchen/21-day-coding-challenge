4b23c34f346fa14b3327d139f3889de64229f6b8
Steven Chen <steven.chen.121@gmail.com>
2021-12-06 11:51:13 -0500

    Challenge #15

--- script.js
+++ script.js
@@ -427,3 +427,34 @@ const switchAllTogglesOn = (toggleList) => {
     toggleList.forEach(toggle => toggle.isOn = true);
     return toggleList;
 }
+
+// ===========================================
+// Challenge #15 - A fated reunion
+// ===========================================
+
+// Instructions
+
+// Your task is to write a function that will take in a launch date and a mission name as strings.Calculate the difference between two dates in days and return an object containing the name of the mission and a rounded day difference.
+
+// A part of the code has been written for you, and it is important that it stays the same for the tests to work out.
+
+// Examples
+// Input:
+
+// const launchDate = "2021-12-12"
+// const fakeToday = "2021-12-01"
+// const missionName = "Moon visit"
+
+// Output:
+
+// {
+//     missionName = "Moon visit",
+//         daysRemaining = 11
+// }
+
+const timeRemaining = (launchDate, missionName, fakeToday) => {
+    const today = fakeToday || new Date() // Do not alter this line!
+
+    const daysRemaining = Math.round((Date.parse(launchDate) - Date.parse(today)) / 86400000);
+    return { missionName: missionName, daysRemaining: daysRemaining }
+}
