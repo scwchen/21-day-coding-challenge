d44bc23879477edaeced2e78db2dfaaeaca7f48b
Steven Chen <steven.chen.121@gmail.com>
2021-12-10 17:01:10 -0500

    Challenge #19

--- script.js
+++ script.js
@@ -572,3 +572,45 @@ const chooseLunchWinner = (listOfChoices) => {
     }, {});
     return Object.keys(choiceFrequency).reduce((a, b) => choiceFrequency[a] > choiceFrequency[b] ? a : b);
 }
+
+// ===========================================
+// Challenge #19 - It's Pumpkin Spice Time
+// ===========================================
+
+// Instructions
+
+// Your task is to write a function that will take in a list of data entries in the shape of an array of objects.The function will then organize and return the entries by type and store each bit of data(string) in each list, in the shape of an object containing arrays.
+
+//     Examples
+// Input:
+
+// const listOfReceivedData = [
+//     { type: "astro", data: "Saturn Data" },
+//     { type: "bio", data: "Space Potatoes" },
+//     { type: "physics", data: "Lagrange Points" },
+//     { type: "bio", data: "OMG Tardigrades" },
+//     { type: "physics", data: "Material reflectivity" },
+//     { type: "astro", data: "Mercury is not the hottest" },
+// ]
+
+// Output:
+
+// {
+//     "astro": ["Saturn Data", "Mercury is not the hottest"],
+//         "bio": ["Space Potatoes", "OMG Tardigrades"],
+//             "physics": ["Lagrange Points", "Material reflectivity"],
+// }
+
+
+// This is not as elegant as I would have wanted it but it works.
+
+const organizeData = (receivedData) => {
+    return receivedData.reduce((object, entry) => {
+        if (object[entry.type]) {
+            object[entry.type] = [...object[entry.type], entry.data];
+        } else {
+            object[entry.type] = [entry.data];
+        }
+        return object;
+    }, {});
+}
