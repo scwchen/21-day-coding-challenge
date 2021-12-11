f5103757fae82c78e34bb8558646fd73faabfc89
Steven Chen <steven.chen.121@gmail.com>
2021-12-11 13:45:00 -0500

    Challenge # 20

--- script.js
+++ script.js
@@ -614,3 +614,51 @@ const organizeData = (receivedData) => {
         return object;
     }, {});
 }
+
+// ===========================================
+// Challenge #20 - Incoming Task: Confirm Re-Entry!
+// ===========================================
+
+// Instructions
+//     (This challenge is worth 10 points)
+
+// Your task is to write a function that will take in speed(number), missionData(object) and checks(object) as parameters.The goal is to make sure that the speed is within the limits and that the amount of entries per type matches with the checks.If one of the values is a mismatch, return false, if everything is fine, return true.
+
+// Speed will be compared against maxSpeed and minSpeed inclusively and the length of each array inside missionData will be compared to the values inside the dataEntries object values.
+
+//     Examples
+// Input:
+
+// const speed = 40
+// const missionData = {
+//     astro: ["...", "..."],
+//     bio: ["..."],
+//     physics: ["..."]
+// }
+
+// const checks = {
+//     maxSpeed: 50,
+//     minSpeed: 20,
+//     dataEntries: {
+//         astro: 3,
+//         bio: 1,
+//         physics: 1
+//     }
+// }
+
+// Output:
+
+// false // Not the same amount of entries
+
+// Again not particularly elegant but it works.
+
+const confirmReentryPlans = (speed, missionData, checks) => {
+    const atSpeed = (speed > checks.minSpeed && speed < checks.maxSpeed);
+    let correctEntries = true;
+
+    for (const type in missionData) {
+        correctEntries = correctEntries && (missionData[type].length === checks.dataEntries[type]);
+    }
+
+    return atSpeed && correctEntries;
+}
