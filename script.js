8cb7cee54942925b0b65fd60b12ba2058ba4900c
Steven Chen <steven.chen.121@gmail.com>
2021-12-12 01:16:03 -0500

    Challenge # 21

--- script.js
+++ script.js
@@ -662,3 +662,58 @@ const confirmReentryPlans = (speed, missionData, checks) => {
 
     return atSpeed && correctEntries;
 }
+
+// ===========================================
+// Challenge #21 - 21 Challenges Later
+// ===========================================
+
+// Instructions
+//     (This challenge is worth 20 points)
+
+// Your last(!) task is to write a function that will take in an array of exchanged messages and an array of organized data.The goal is to parse each message, add them to a list and return an object containing two keys, transcript with the messages, missionData with the missionData object.
+
+// Each message should be parsed using the same structure as the first challenge;
+
+// "Origin: Message"
+//     .
+// You can reuse the function of the first challenge if you want!
+
+// Examples
+// Input:
+
+// const exchanges = [
+//     { origin: "MC", message: "So how is it out there?" },
+//     { origin: "Shuttle", message: "Oh it's pretty nice!" },
+//     { origin: "MC", message: "Did you like the challenges?" },
+// ]
+
+// const missionData = {
+//     astro: ["...", "..."],
+//     bio: ["..."],
+//     physics: ["..."]
+// }
+
+// Output:
+
+// {
+//     transcript:
+//     [
+//         "MC: So how is it out there?",
+//         "Shuttle: Oh it's pretty nice!",
+//         "MC: Did you like the challenges?"
+//     ],
+//         missionData: {
+//         astro: ["...", "..."],
+//             bio: ["..."],
+//                 physics: ["..."]
+//     }
+// }
+
+const parseMissionSummary = (exchanges, missionData) => {
+    return {
+        transcript: exchanges.map(exchange => `${exchange.origin}: ${exchange.message}`),
+        missionData: missionData,
+    }
+}
+
+// And that's a wrap
